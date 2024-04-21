492e1b406d40f914a9efc2492859d4bca15c226d
Siavash Ameli <sia.sunrise@gmail.com>
2024-04-20 19:33:31 -0700

    init

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setuptools.setup(
     version="0.0.1",
     author="Siavash Ameli",
     author_email='sameli@berkeley.edu',
-    description='Enhance your matplotlib plots with publication-quality ' +
-                'styles',
+    description='Enhance your matplotlib plots with publication-quality style',
     long_description="texplot",
     long_description_content_type='text/x-rst',
 )
