c4be3c7f578efd3feba709afbf31f94f3eb9c01b
Siavash Ameli <sia.sunrise@gmail.com>
2025-06-19 14:38:57 -0700

    i snotebook

--- texplot/__init__.py
+++ texplot/__init__.py
@@ -13,9 +13,10 @@
 
 from .plot_utilities import theme, get_theme, set_theme, reset_theme, \
     save_plot, show_or_save_plot
+from .display_utilities import is_notebook
 from . import examples
 
 __all__ = ['theme', 'get_theme', 'set_theme', 'reset_theme',
-           'save_plot', 'show_or_save_plot', 'examples']
+           'save_plot', 'show_or_save_plot', 'examples', 'is_notebook']
 
 from .__version__ import __version__                          # noqa: F401 E402
